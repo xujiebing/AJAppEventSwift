5e67ffd67970a80a94b0b266290b7b0997f80ddb
xujiebing <xujiebing1992@gmail.com>
2019-12-06 14:53:20 +0800

    提交代码

--- Example/Pods/Target Support Files/Pods-AJAppEventSwift_Example/Pods-AJAppEventSwift_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-AJAppEventSwift_Example/Pods-AJAppEventSwift_Example-frameworks.sh
@@ -162,9 +162,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AJAppEventSwift/AJAppEventSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AJKitSwift/AJKitSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AJAppEventSwift/AJAppEventSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AJKitSwift/AJKitSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
